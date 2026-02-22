cbad38fe749d903d924edfc47d46fc1117525d2a
Joos <jooshuys@hotmail.com>
2026-02-22 23:27:45 +0100

    test(overview): add unit tests for loading failure notification component.

---
+++ tests/unit/presentation/components/loading-failure-notification.test.ts
@@ -0,0 +1,40 @@
+import { LoadingStatuses } from "@/models/loading";
+import LoadingFailureNotificationCode from "@/presentation/components/loading-failure-notification/loading-failure-notification.code";
+
+const mocksStore: any = vi.hoisted(() => {
+	const storeVariables = {
+		loadingProcess: { status: 0, errorMessage: "" }
+	};
+
+	return {
+		getLoadingProcess: () => storeVariables.loadingProcess
+	};
+});
+
+vi.mock("@/store", () => ({
+	__esModule: true,
+	default: {
+		getters: {
+			loadingProcessOfType: () => mocksStore.getLoadingProcess()
+		}
+	}
+}));
+
+describe("loading failure notification", () => {
+	let code: LoadingFailureNotificationCode;
+
+	beforeEach(() => {
+		code = new LoadingFailureNotificationCode();
+	});
+
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
+
+	it("loadingProcess: when loading status %#, give expected value.", () => {
+		expect(code.loadingProcess.value).toEqual({
+			status: LoadingStatuses.INACTIVE,
+			errorMessage: ""
+		});
+	});
+});
